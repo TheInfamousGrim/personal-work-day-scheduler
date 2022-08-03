bd9b8fd33ee51ebc0e98338371f9a41775e3a2e2
TheInfamousGrim <finchergeorge1@gmail.com>
2022-08-03 20:06:40 +0100

    created function to save scheduled events to local storage

--- Develop/js/schedule.js
+++ Develop/js/schedule.js
@@ -7,7 +7,7 @@ const saveBtns = document.querySelectorAll('.saveBtn');
 
 /* --------------------------- display current day -------------------------- */
 // Grab current date and format it
-const currentDayJS = dayjs().format('dddd, MMMM, DD');
+const currentDayJS = dayjs().format('dddd, MMMM, DD, YYYY');
 // Update the currentDayText with the currentDayJS
 currentDayText.text(currentDayJS);
 
@@ -22,28 +22,48 @@ function eventColorize() {
 
         if (currentHour > dataHour) {
             txtArea.classList.remove('present');
-            txtArea.classList.remove('future');
             txtArea.classList.add('past');
         } else if (currentHour === dataHour) {
-            txtArea.classList.remove('past');
             txtArea.classList.remove('future');
             txtArea.classList.add('present');
         } else {
-            txtArea.classList.remove('present');
             txtArea.classList.remove('past');
             txtArea.classList.add('future');
         }
     });
 }
 
-const eventIntervalState = setInterval(eventColorize, 1000);
+eventColorize();
+/* ---------------------- add scheduled event function ---------------------- */
+
+function addScheduledEvent(array, eventDescription, eventHour, eventDate) {
+    // get the length of the array
+    const { length } = array;
+    // increment the id value in the object to be added
+    const id = length + 1;
+    // check if there's an event happening at the same date and time
+    const found = array.some((el) => el.event_hour === eventHour && el.event_date === eventDate);
+    // Add the event to the array if there's no event conflict
+    if (!found) array.push({ id, event_description: eventDescription, event_hour: eventHour, event_date: eventDate });
+    // Add the array of events to the local storage
+    localStorage.setItem('scheduledEvents', JSON.stringify(array));
+}
 
 /* ------------------------- save the current event ------------------------- */
 
 saveBtns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        console.log(e.currentTarget);
+        // get current event to be added text
+        const currentEventText = e.currentTarget.previousElementSibling.value;
+        // get current event to be added data hour
+        const currentEventHour = e.currentTarget.previousElementSibling.getAttribute('data-hour');
+
+        // get a reference to the events in the local storage
+        const eventsForCurrentDay = JSON.parse(localStorage.getItem('scheduledEvents')) || [];
+
+        // if the event text is not empty add the scheduled event
+        if (currentEventText !== '') {
+            addScheduledEvent(eventsForCurrentDay, currentEventText, currentEventHour, currentDayText.text());
+        }
     });
 });
-
-// Save the event to the local storage for that day
